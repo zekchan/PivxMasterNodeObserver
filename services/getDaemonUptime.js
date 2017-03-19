dcafa01f297e2c27f837ef936e537cb8a1cfead4
Ilya Groshev <groshev.i.d@gmail.com>
2017-03-19 21:08:49 +0300

    Daemon uptime added to info

--- services/getDaemonUptime.js
+++ services/getDaemonUptime.js
@@ -3,7 +3,7 @@ const exec = require('./exec');
 module.exports = () => exec('pgrep pivxd')
     .then(pid => {
         if (pid) {
-            return exec(`ps -p "${pid}" -o etime=`);
+            return exec(`ps -p "${pid.trim()}" -o etime=`);
         }
 
         return '';
